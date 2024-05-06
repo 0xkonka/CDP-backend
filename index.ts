2eaa1e59965a37c87365f8e55a80657274e69c26
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-05-05 19:11:23 -0500

    fix cors policy

--- index.ts
+++ index.ts
@@ -9,11 +9,20 @@ dotenv.config()
 
 const app = express()
 const port = process.env.PORT
-const options = {
-  origin: '*',
-}
+const corsOptions = {
+  origin: ['https://tren-staging.vercel.app', 'http://localhost:3000'], // Allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], // Allowed headers
+};
 
-app.use(cors(options))
+app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  res.set('X-Content-Type-Options', 'nosniff');
+  res.set('X-Frame-Options', 'DENY');
+  res.set('Referrer-Policy', 'same-origin');
+  next();
+});
+
 db.connect();
 // app.use(express.json());
 // app.use(function (_, res, next) {

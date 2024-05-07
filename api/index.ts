c65ed4fb385497e652577a89af22b83346210f16
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-05-07 08:15:38 -0500

    fix swagger ui option

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-import swaggerUi from 'swagger-ui-express'
-import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 import routes from '../src/routes'
 import db from '../src/db'
@@ -14,11 +14,12 @@ const port = process.env.PORT || 8000
 
 const swaggerOptions = {
   definition: {
-    swagger: '2.0',
+    openapi: '3.0.0',
     info: {
       title: 'Tren Express API',
       version: '1.0.0',
-      description: 'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+      description:
+        'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
       license: {
         name: 'Licensed Under MIT',
         url: 'https://spdx.org/licenses/MIT.html',
@@ -40,65 +41,51 @@ const swaggerOptions = {
     ],
   },
   // Paths to files containing OpenAPI definitions
-  apis: [`src/routes/index.ts`],
-}
+  apis: ['dist/src/routes/index.js'],
+};
 
-// [`${__dirname}/routes/abc.js`]
-
-const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.2/swagger-ui.min.css'
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions)
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 const corsOptions = {
   // origin: ['https://tren-staging.vercel.app', 'http://localhost:3000'], // TODO: use this in final version
-  origin: function (origin: any, callback: any) {
-    if (!origin) {
-      callback(null, true)
-    } else if (origin.match(/^.+\.vercel\.app$/) || origin === 'http://localhost:3000') {
+  origin: function (origin : any, callback : any) {
+    if(!origin){
+      callback(null, true);
+    }
+    else if (origin.match(/^.+\.vercel\.app$/) || origin === 'http://localhost:3000') {
       // Allow if it's a Vercel deployment or local development
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], // Allowed headers
-}
+};
 app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use((req, res, next) => {
-  res.set('X-Content-Type-Options', 'nosniff')
-  res.set('X-Frame-Options', 'DENY')
-  res.set('Referrer-Policy', 'same-origin')
-  next()
-})
+  res.set('X-Content-Type-Options', 'nosniff');
+  res.set('X-Frame-Options', 'DENY');
+  res.set('Referrer-Policy', 'same-origin');
+  next();
+});
+
+db.connect()
 
 // API routes
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, {
-    customCss:
-      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-    customCssUrl: CSS_URL,
-    explorer: true
-  })
-)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/swagger.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.send(swaggerSpec)
-})
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 app.get('/ping', (req, res, next) => {
   res.send('ok')
   next()
 })
-
-app.use(express.static("./"));
-
-db.connect()
 
 routes(app)
 

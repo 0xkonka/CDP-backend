0c2bfbcc74147145aef72127a832fbddb5bda147
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-05 13:37:07 -0400

    revert

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const app = express()
 
 app.use(bodyParser.json()) // to use body object in requests
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors(corsOptions))
 // app.use(cors())
 app.use((req, res, next) => {
   res.set('X-Content-Type-Options', 'nosniff')

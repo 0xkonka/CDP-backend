15c9f17533355cc0ad223c6f31902f943b5f60fa
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-18 12:55:32 -0400

    add cors

--- src/middleware/validateRealIp.js
+++ src/middleware/validateRealIp.js
@@ -4,8 +4,13 @@ const allowedRealIp = '167.71.105.201'
 
 export const validateRealIp = (req, res, next) => {
   const realIp = req.headers['x-real-ip']
+  const origin = req.headers.origin
 
   console.log('x-real-ip', req.headers)
+
+  if (origin && origin == 'https://miniapp.tren.finance') {
+    return next()
+  }
 
   if (realIp !== allowedRealIp) {
     return res.status(403).json({ error: 'Forbidden' })

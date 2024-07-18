d7bbdf3adc58aadfd7fa70e54bf52b05c9b6f427
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-18 12:48:21 -0400

    add cors

--- src/middleware/validateRealIp.js
+++ src/middleware/validateRealIp.js
@@ -5,7 +5,7 @@ const allowedRealIp = '167.71.105.201'
 export const validateRealIp = (req, res, next) => {
   const realIp = req.headers['x-real-ip']
 
-  console.log('x-real-ip', req.headers['x-real-ip'])
+  console.log('x-real-ip', req.headers)
 
   if (realIp !== allowedRealIp) {
     return res.status(403).json({ error: 'Forbidden' })

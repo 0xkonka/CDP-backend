0dd4fa31d3a52113ac75c7489913117f52c37e83
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-07-18 17:58:48 +0900

    update cors

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import swaggerUI from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
-import bodyParser from 'body-parser';
-import apiRouter from './src/Routes/index.js';
-import helloRouter from './src/hello.js';
-import db from './src/db/index.js';
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import swaggerUI from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
+import bodyParser from 'body-parser'
+import apiRouter from './src/Routes/index.js'
+import helloRouter from './src/hello.js'
+import db from './src/db/index.js'
 
-dotenv.config();
-const PORT = process.env.PORT || 8000;
+dotenv.config()
+const PORT = process.env.PORT || 8000
 
 const allowedOrigins = [
   'https://tren.finance',
@@ -26,29 +26,29 @@ const allowedOrigins = [
   'https://miniapp.tren.finance',
   'https://telegram-mini-app-kappa.vercel.app',
   'http://localhost:8000',
-];
+]
 
-const allowedRealIp = '167.71.105.201';
+const allowedRealIp = '167.71.105.201'
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps, curl requests)
+    if (!origin) return callback(null, true) // Allow requests with no origin (like mobile apps, curl requests)
     if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false)
     }
-    return callback(null, true);
+    return callback(null, true)
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
   credentials: true,
-};
+}
 
-const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.2/swagger-ui.min.css';
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.2/swagger-ui.min.css'
 let customCss = `
   .swagger-ui .opblock .opblock-summary-path { max-width: 100% !important} 
   .swagger-ui .opblock .opblock-summary-description { padding: 0 10px }
-`;
+`
 
 const swaggerOptions = {
   definition: {
@@ -70,51 +70,55 @@ const swaggerOptions = {
     ],
   },
   apis: ['src/**/*.js'],
-};
+}
 
-const specs = swaggerJsDoc(swaggerOptions);
+const specs = swaggerJsDoc(swaggerOptions)
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(morgan('dev'));
-app.use(cors(corsOptions));
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(cors(corsOptions))
 app.use((req, res, next) => {
-  res.set('X-Content-Type-Options', 'nosniff');
-  res.set('X-Frame-Options', 'DENY');
-  res.set('Referrer-Policy', 'same-origin');
-  next();
-});
+  res.set('X-Content-Type-Options', 'nosniff')
+  res.set('X-Frame-Options', 'DENY')
+  res.set('Referrer-Policy', 'same-origin')
+  next()
+})
 
 // Middleware to validate Origin or x-real-ip headers, or allow Swagger UI access
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  const realIp = req.headers['x-real-ip'];
-  const reffer = req.headers.referer;
+  const origin = req.headers.origin
+  const realIp = req.headers['x-real-ip']
+  const reffer = req.headers.referer
 
-  console.log('origin', origin);
-  console.log('x-real-ip', realIp);
+  console.log('origin', origin)
+  console.log('x-real-ip', realIp)
   console.log('reffer', reffer)
-  
-  if (req.path.startsWith('/api-docs') || reffer.includes('/api-docs')) {
-    return next(); // Allow requests to the Swagger UI
+
+  if (req.path.startsWith('/api-docs')) {
+    return next() // Allow requests to the Swagger UI
+  }
+
+  if (reffer && reffer.includes('/api-docs')) {
+    return next()
   }
 
   if (origin && allowedOrigins.includes(origin)) {
-    return next();
+    return next()
   }
 
   if (realIp === allowedRealIp) {
-    return next();
+    return next()
   }
 
-  return res.status(403).json({ error: 'Forbidden' });
-});
+  return res.status(403).json({ error: 'Forbidden' })
+})
 
-db.connect();
+db.connect()
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs, { customCssUrl: CSS_URL, customCss }));
-app.use('/', helloRouter);
-app.use('/api', apiRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs, { customCssUrl: CSS_URL, customCss }))
+app.use('/', helloRouter)
+app.use('/api', apiRouter)
 
-app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server runs on port ${PORT}`))
